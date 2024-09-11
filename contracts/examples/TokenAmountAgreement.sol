5cd71b9df26750de14a620a353a8905496a59c63
MASDXI <sirawitt42@gmail.com>
2024-09-11 22:04:03 +0700

    refactor: little bit comment and file naming
    Signed-off-by: MASDXI <sirawitt42@gmail.com>

---
+++ contracts/examples/TokenAmountAgreement.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.8.0;
+
+/// @title General Token Amount Agreement
+/// @notice This contract serves as a template for creating bilateral agreements based on token amounts.
+///         It facilitates programmable agreements between two parties involving the exchange or validation
+///         of token amounts.
+/// @dev Documentation and implementation details of the General Token Amount Agreement
+///      are available at https://github.com/Kiwari-labs/
+/// @author Kiwari-labs
+
+/// @TODO change to 
+// import @kiwarilabs/contracts/libraries/comparator/AddresComparator.sol";
+// import @kiwarilabs/contracts/libraries/comparator/IntComparator.sol";
+// import @kiwarilabs/contracts/agreement/bilateral/AgreementTemplate.sol";
+
+import "../AddressComparator.sol";
+import "../IntComparator.sol";
+import "../template/AgreementTemplate.sol";
+
+contract GeneralTokenAgreement is AgreementTemplate {
+    using IntComparator for uint256;
+    using AddressCompartor for address;
+
+    function _verifyAmountToken(
+        uint inputTokenAmount,
+        uint requiredAmount
+    ) private pure returns (bool) {
+        return inputTokenAmount.equal(requiredAmount);
+    }
+
+    function _verifyBalanceToken(
+        address token,
+        address bilateralAgreementContract,
+        uint requiredAmountToken
+    ) private pure returns (bool) {
+        return
+            (IERC20(token).balanceOf(bilateralAgreementContract)).equal(
+                requiredAmountToken
+            );
+    }
+
+    function _verifyBilaterlContract(
+        address contractA,
+        address contractB
+    ) private pure returns (bool) {
+        return contractA.equal(contractB);
+    }
+
+    function _verifyAgreement(
+        bytes memory x,
+        bytes memory y
+    ) internal override returns (bool) {
+        // #################### start your custom logic ####################
+        // Decode amounts and addresses from both parties
+        (
+            uint amountTokenA,
+            uint requiredAmountTokenB,
+            address tokenA,
+            address agreementContractA
+        ) = abi.decode(x, (uint, uint, address, address));
+        (
+            uint amountTokenB,
+            uint requiredAmountTokenA,
+            address tokenB,
+            address agreementContractB
+        ) = abi.decode(y, (uint, uint, address, address));
+        // Verify contract addresses, token amounts, and balances
+        require(
+            _verifyBilaterlContract(agreementContractA, agreementContractB),
+            "Invalid agreement contract"
+        );
+        require(
+            _verifyAmountToken(amountTokenA, requiredAmountTokenA) &&
+                _verifyAmountToken(amountTokenB, requiredAmountTokenB),
+            "Invalid token amounts"
+        );
+        require(
+            _verifyBalanceToken(amountTokenA, agreementContractA) &&
+                _verifyBalanceToken(amountTokenB, agreementContractB),
+            "Invalid token balances"
+        );
+        // #################### end your custom logic ####################
+        // do not change the line below.
+        return true;
+    }
+}

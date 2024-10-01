d3db348a4044b7feba3fa34d48f926f64d1a0a90
sirawt <31649128+MASDXI@users.noreply.github.com>
2024-10-01 21:07:00 +0700

    Create AgreementTemp.sol
    Signed-off-by: sirawt <31649128+MASDXI@users.noreply.github.com>

---
+++ contracts/examples/AgreementTemp.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+/// @title Cutomized Agreement
+/// @author <author@domain.com>
+
+import "@kiwarilabs/contracts/abstracts/AgreementTemplate.sol";
+
+contract CutomizedAgreement is AgreementTemplate {
+    function _verifyAgreement(
+        bytes memory x,
+        bytes memory y
+    ) internal override returns (bool) {
+        // #################### start your custom logic ####################
+        // ... desirable logic here
+        // #################### end your custom logic ####################
+        // do not change the line below.
+        return true;
+    }
+}
